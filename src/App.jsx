ea577e9497cf80447884c823ebdd5f8df9997a76
An <an.nguyen@webdesigner.kiwi>
2025-08-25 19:07:42 +1200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CheckoutScreen from './components/CheckoutScreen'
 import KairoWebsite from './components/KairoWebsite'
 import Success from './pages/success.jsx'
 import Cancel from './pages/cancel.jsx'
+import CompletePayment from './pages/CompletePayment.jsx'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/checkout" element={<CheckoutScreen />} />
       <Route path="/success" element={<Success />} />
       <Route path="/cancel" element={<Cancel />} />
+      <Route path="/complete" element={<CompletePayment />} />
     </Routes>
   )
 }
